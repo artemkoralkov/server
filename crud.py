79f8dcc33d2d5e879a8c58678dfe3e47c01d4f17
Koralkov Artem <artemkoralkov@gmail.com>
2022-10-03 19:11:08 +0300

    fix edit lesson

--- crud.py
+++ crud.py
@@ -245,5 +245,5 @@ def delete_lesson(db: Session, lesson_id):
     db.commit()
 
 def edit_lesson(db: Session, lesson_id, lesson):
-    print(lesson)
+    print(models.Lesson(**lesson))
     # db.query(models.Lesson).filter(models.Lesson.id == lesson_id).update(**lesson)
