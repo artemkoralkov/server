fed105b267603d6467e06319549a6c9f5e3e030c
Koralkov Artem <artemkoralkov@gmail.com>
2022-10-03 19:07:01 +0300

    refactor comment editlesson

--- crud.py
+++ crud.py
@@ -6,7 +6,6 @@ Return: return_description
 """
 
 from itertools import groupby
-from operator import mod
 from uuid import uuid4
 from typing import List
 from sqlalchemy.orm import Session
@@ -246,4 +245,5 @@ def delete_lesson(db: Session, lesson_id):
     db.commit()
 
 def edit_lesson(db: Session, lesson_id, lesson):
-    db.query(models.Lesson).filter(models.Lesson.id == lesson_id).update(**lesson)
+    print(lesson)
+    # db.query(models.Lesson).filter(models.Lesson.id == lesson_id).update(**lesson)
