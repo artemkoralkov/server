f3b767f39ca9520aeb227b6b5c28ace120909cbd
Artem Koralkov <artemkoralkov@gmail.com>
2023-03-09 11:40:13 +0300

    fix teacher lesson equality chek

--- modules/lessons/utils/is_teahcer_lessons_equal.py
+++ modules/lessons/utils/is_teahcer_lessons_equal.py
@@ -1,7 +1,6 @@
 def is_teahcer_lessons_equal(first_lesson, second_lesson):
     return first_lesson['numerator'] == second_lesson['numerator'] and \
         first_lesson['denominator'] == second_lesson['denominator'] and \
-        first_lesson['faculty'] == second_lesson['faculty'] and \
         first_lesson['denominator'] == second_lesson['denominator'] and \
         first_lesson['teacher_name'] == second_lesson['teacher_name'] and \
         first_lesson['first_group'] == second_lesson['first_group'] and \
