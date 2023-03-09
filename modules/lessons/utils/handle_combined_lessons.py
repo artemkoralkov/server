964c04637ec0043f33e1c1c29bd04b3f83a76693
Koralkov Artem <artemkoralkov@gmail.com>
2023-03-09 19:56:33 +0300

    fix

--- modules/lessons/utils/handle_combined_lessons.py
+++ modules/lessons/utils/handle_combined_lessons.py
@@ -21,7 +21,6 @@ def handle_combined_lessons(lessons):
         first_lesson_dict = lessons[0].__dict__
         second_lesson_dict = lessons[1].__dict__
         third_lesson_dict = lessons[2].__dict__
-        print(first_lesson_dict, second_lesson_dict, third_lesson_dict)
         if is_teahcer_lessons_equal(first_lesson_dict, second_lesson_dict) and not is_teahcer_lessons_equal(first_lesson_dict, third_lesson_dict):
             if first_lesson_dict["group_name"][:3] + first_lesson_dict["group_name"][5:] == second_lesson_dict["group_name"][:3] + second_lesson_dict["group_name"][5:]:
                 group = first_lesson_dict["group_name"][:3] + first_lesson_dict["group_name"][5:]
