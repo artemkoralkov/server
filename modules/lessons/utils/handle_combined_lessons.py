d7eb853d349b6921be11ea2d728b2391eb2b97c3
Artem Koralkov <artemkoralkov@gmail.com>
2023-03-10 11:08:39 +0300

    fix handle combined lesson function

--- modules/lessons/utils/handle_combined_lessons.py
+++ modules/lessons/utils/handle_combined_lessons.py
@@ -2,6 +2,7 @@ from .is_teahcer_lessons_equal import is_teahcer_lessons_equal
 from ..models import Lesson
 
 def handle_combined_lessons(lessons):
+    print(lessons)
     if len(lessons) == 2:
         first_lesson_dict = lessons[0].__dict__
         second_lesson_dict = lessons[1].__dict__
@@ -21,6 +22,7 @@ def handle_combined_lessons(lessons):
         first_lesson_dict = lessons[0].__dict__
         second_lesson_dict = lessons[1].__dict__
         third_lesson_dict = lessons[2].__dict__
+        print(first_lesson_dict, second_lesson_dict, third_lesson_dict)
         if is_teahcer_lessons_equal(first_lesson_dict, second_lesson_dict) and not is_teahcer_lessons_equal(first_lesson_dict, third_lesson_dict):
             if first_lesson_dict["group_name"][:3] + first_lesson_dict["group_name"][5:] == second_lesson_dict["group_name"][:3] + second_lesson_dict["group_name"][5:]:
                 group = first_lesson_dict["group_name"][:3] + first_lesson_dict["group_name"][5:]
@@ -47,15 +49,16 @@ def handle_combined_lessons(lessons):
         elif is_teahcer_lessons_equal(second_lesson_dict, third_lesson_dict) and not is_teahcer_lessons_equal(first_lesson_dict, second_lesson_dict):
             if second_lesson_dict["group_name"][:3] + second_lesson_dict["group_name"][5:] == \
              third_lesson_dict["group_name"][:3] + third_lesson_dict["group_name"][5:]:
-                group = second_lesson_dict["group_name"][:3] + first_lesson_dict["group_name"][5:]
+                group = second_lesson_dict["group_name"][:3] + second_lesson_dict["group_name"][5:]
             else:
                 group = f'{second_lesson_dict["group_name"]}, {third_lesson_dict["group_name"]}'
             lesson_id = f'{second_lesson_dict["id"]}, {third_lesson_dict["id"]}'
+            print(group)
             del second_lesson_dict["id"]
             del second_lesson_dict['group_name']
             del second_lesson_dict['_sa_instance_state']
             del first_lesson_dict['_sa_instance_state']
-            return [Lesson(id=lesson_id, group_name=group, **second_lesson_dict), Lesson(**first_lesson_dict)]
+            return [Lesson(**first_lesson_dict), Lesson(id=lesson_id, group_name=group, **second_lesson_dict)]
         elif is_teahcer_lessons_equal(first_lesson_dict, second_lesson_dict) and is_teahcer_lessons_equal(first_lesson_dict, third_lesson_dict):
             group = f'{first_lesson_dict["group_name"]}, {second_lesson_dict["group_name"]}, {third_lesson_dict["group_name"]}'
             lesson_id = f'{first_lesson_dict["id"]}, {second_lesson_dict["id"]}, {third_lesson_dict["id"]}'
@@ -65,5 +68,18 @@ def handle_combined_lessons(lessons):
             return [Lesson(id=lesson_id, group_name=group, **first_lesson_dict)]
         else:
             return lessons
- 
+    elif len(lessons) == 4:
+        first_lesson_dict = lessons[0].__dict__
+        second_lesson_dict = lessons[1].__dict__
+        third_lesson_dict = lessons[2].__dict__
+        fourth_lesson_dict = lessons[3].__dict__
+        if is_teahcer_lessons_equal(first_lesson_dict, second_lesson_dict) and is_teahcer_lessons_equal(first_lesson_dict, third_lesson_dict) and is_teahcer_lessons_equal(first_lesson_dict, fourth_lesson_dict):
+            group = f'{first_lesson_dict["group_name"]}, {second_lesson_dict["group_name"]}, {third_lesson_dict["group_name"]}, {fourth_lesson_dict["group_name"]}'
+            lesson_id = f'{first_lesson_dict["id"]}, {second_lesson_dict["id"]}, {third_lesson_dict["id"]}, {fourth_lesson_dict["id"]}'
+            del first_lesson_dict["id"]
+            del first_lesson_dict['group_name']
+            del first_lesson_dict['_sa_instance_state']
+            return [Lesson(id=lesson_id, group_name=group, **first_lesson_dict)]
+        else:
+            return lessons
         
