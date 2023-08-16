46bb4e88ca40dd28d6cca7cc98b73ec032a3de56
Koralkov Artem <artemkoralkov@gmail.com>
2023-08-16 17:04:39 +0300

    refactor

--- modules/rooms/schemas.py
+++ modules/rooms/schemas.py
@@ -19,7 +19,7 @@ class RoomReservation(RoomReservationBase):
 
     # lessons: 'list[Lesson]' = []
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class RoomBase(BaseModel):
