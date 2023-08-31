40fbe470ad57448ad70fb032e998489b39f3902d
Koralkov Artem <artemkoralkov@gmail.com>
2023-08-31 18:00:57 +0300

    connect to postgres

--- src/database.py
+++ src/database.py
@@ -5,12 +5,12 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from src.config import settings
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///sql_app.db"
-SQLALCHEMY_DATABASE_URL = settings.SQLITE_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = settings.POSTGRES_DATABASE_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
